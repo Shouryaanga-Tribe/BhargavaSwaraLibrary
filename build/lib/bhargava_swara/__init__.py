f7f8494655509a8844993915045b2e8a1098ff57
kckDeepak <kckdeepak29@gmail.com>
2025-04-06 11:55:11 +0530

    Added 3 more spectograms, updated README.md file.

--- build/lib/bhargava_swara/__init__.py
+++ build/lib/bhargava_swara/__init__.py
@@ -5,6 +5,9 @@ from .carnatic_or_hindustani import analyze_tradition
 from .ornament_detection import analyze_ornaments
 from .real_time_full_analysis import analyze_music_full
 from .mel_spectrogram import generate_mel_spectrogram
+from .chroma_spectrogram import generate_chroma_spectrogram
+from .cqt_spectrogram import generate_cqt_spectrogram
+from .cent_spectrogram import generate_cent_spectrogram
 
 __all__ = [
     "analyze_raga",
@@ -13,5 +16,8 @@ __all__ = [
     "analyze_tradition",
     "analyze_ornaments",
     "analyze_music_full",
-    "generate_mel_spectrogram"
+    "generate_mel_spectrogram",
+    "generate_chroma_spectrogram",
+    "generate_cqt_spectrogram",
+    "generate_cent_spectrogram"
 ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bhargava_swara",
-    version="0.0.7",
+    version="0.0.9",
     author="Kuchi Chaitanya Krishna Deepak",
     author_email="kckdeepak29@gmail.com",
     description="A library for analysis and synthesis of Indian classical music",
